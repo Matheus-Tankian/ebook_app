a2fc412bce3ce76537cccacc675301c0d621b812
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2021-11-10 15:55:54 -0300

    books grid criado

--- lib/pages/home/widgets/book_staggered_grid_view.dart
+++ lib/pages/home/widgets/book_staggered_grid_view.dart
@@ -1,19 +1,41 @@
+import 'package:ebook_app/models/book.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'book_item.dart';
 
 class BookStaggeredGridView extends StatelessWidget {
   final int selected;
   final PageController pageController;
   final Function callback;
-  const BookStaggeredGridView(
+  BookStaggeredGridView(
       this.selected,
       this.pageController,
       this.callback,
-      {Key? key}) : super(key: key
-  );
+      {Key? key}) : super(key: key);
+
+  final bookList = Book.generateBooks();
 
   @override
   Widget build(BuildContext context) {
-    return Text('BookStaggeredGridView');
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: PageView(
+        controller: pageController,
+        onPageChanged: (index) => callback(index),
+        children: [
+         StaggeredGridView.countBuilder(
+           shrinkWrap: true,
+           physics: const ScrollPhysics(),
+           crossAxisSpacing: 16,
+           mainAxisSpacing: 16,
+           crossAxisCount: 4,
+           itemCount: bookList.length,
+           itemBuilder: (_,index) =>BookItem(bookList[index]),
+           staggeredTileBuilder: (_) => const StaggeredTile.fit(2)),
+          Container(),
+        ],
+      ),
+    );
   }
 }
