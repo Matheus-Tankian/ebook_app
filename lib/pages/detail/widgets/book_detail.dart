210582e02c9300032c12ee41c4a2b9ea49851e5f
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2021-11-25 16:04:53 -0300

    text detail

--- lib/pages/detail/widgets/book_detail.dart
+++ lib/pages/detail/widgets/book_detail.dart
@@ -1,5 +1,7 @@
+import 'package:ebook_app/constants/colors.dart';
 import 'package:ebook_app/models/book.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BookDetail extends StatelessWidget {
   final Book book;
@@ -9,7 +11,57 @@ class BookDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Book detail'),
+     padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(book.type.toUpperCase(),
+            style: const TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(book.name,
+            style: const TextStyle(
+              fontSize: 24,
+              color: kFont,
+              height: 1.2,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text.rich(
+                TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'Published from ',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    TextSpan(
+                      text: book.publisher,
+                      style: const TextStyle(
+                        color: kFont,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                DateFormat.yMMMd().format(book.date),
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
